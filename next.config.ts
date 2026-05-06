34fa5fbfc77f0999ec27fd5441b5c407443881d4
kuro <kkrmlove1011@gmail.com>
2026-05-06 16:19:03 +0900

    🔧 next.config.ts に設定

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // next/image は外部URLの画像をセキュリティのためデフォルトでブロックしている。
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.jp", // ← 許可するドメインを追加
+      },
+    ],
+  },
 };
 
 export default nextConfig;
