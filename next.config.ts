37092217b381ff924614eb2e76ce66b456240b7a
kuro <kkrmlove1011@gmail.com>
2026-05-11 19:59:20 +0900

    🔧microCMSから取得した画像をNext.jsで表示

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // next/image は外部URLの画像をセキュリティのためデフォルトでブロックしている。
+  // next/image は外部URLの画像をセキュリティのためデフォルトでブロックしているため、追記が必要。
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "placehold.jp", // ← 許可するドメインを追加
+        hostname: "placehold.jp",
       },
+      { protocol: "https", hostname: "images.microcms-assets.io" },
     ],
   },
 };
