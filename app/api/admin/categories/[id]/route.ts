cd109744fd408b90b9f123ec1d74e3f8b6b5c8eb
kuro <kkrmlove1011@gmail.com>
2026-07-06 20:49:53 +0900

    ✨ カテゴリー詳細ページ更新

--- app/api/admin/categories/[id]/route.ts
+++ app/api/admin/categories/[id]/route.ts
@@ -37,3 +37,32 @@ export const GET = async (
       return NextResponse.json({ message: error.message }, { status: 400 });
   }
 };
+// ---------- カテゴリー更新 ----------
+export type UpdateCategoryRequestBody = {
+  name: string;
+};
+
+export const PUT = async (
+  request: Request,
+  { params }: { params: Promise<{ id: string }> } //// ここでリクエストパラメータを受け取る
+) => {
+  // paramsの中にidが入っているので、それを取り出す
+  const { id } = await params;
+  // リクエストのbodyを取得
+  const { name }: UpdateCategoryRequestBody = await request.json();
+  try {
+    // idを指定して、Categoryを更新
+    await prisma.category.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        name,
+      },
+    });
+    return NextResponse.json({ message: "OK" }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+};
