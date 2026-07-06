2ba7210c4701189ce266241f905f73e25a40e468
kuro <kkrmlove1011@gmail.com>
2026-07-06 15:35:28 +0900

    💄

--- _components/Header/index.tsx
+++ _components/Header/index.tsx
@@ -5,16 +5,25 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <ul className="flex justify-between">
-        <li>
-          <Link className={styles.header_link} href="/">
-            Blog
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.header_link} href="/contact">
-            お問い合わせ
-          </Link>
-        </li>
+        <div>
+          <li>
+            <Link className={styles.header_link} href="/">
+              Blog
+            </Link>
+          </li>
+        </div>
+        <div className="flex gap-4">
+          <li>
+            <Link className={styles.header_link} href="/contact">
+              お問い合わせ
+            </Link>
+          </li>
+          <li>
+            <Link className={styles.header_link} href="/admin/posts">
+              ログイン
+            </Link>
+          </li>
+        </div>
       </ul>
     </header>
   );
