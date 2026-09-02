46b2e11492ae7b5c99920d7888fd9cdb5325fc67
kuro <kkrmlove1011@gmail.com>
2026-09-02 18:22:19 +0900

    🎨 管理者ページの共通レイアウトコンポーネントの改修

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,25 +1,50 @@
+// 管理者ページの共通レイアウトコンポーネント
+
 "use client";
-import Link from "next/link";
+
 import { ReactNode } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRouteGuard } from "./_hooks/useRouteGuard";
 
 type Props = {
   children: ReactNode;
 };
 
-const adminLayout = ({ children }: Props) => {
+const AdminLayout = ({ children }: Props) => {
+  useRouteGuard();
+
+  const pathname = usePathname();
+  const isSelected = (href: string) => {
+    return pathname.includes(href);
+  };
+
   return (
     <>
+      {/* サイドバー */}
       <aside className="fixed bg-gray-100 w-70 left-0 bottom-0 top-16">
-        <Link href="/admin/posts" className="p-4 block hover:bg-blue-100">
+        <Link
+          href="/admin/posts"
+          className={`p-4 block hover:bg-blue-100 ${
+            isSelected("/admin/posts") && "bg-blue-100"
+          }`}
+        >
           記事一覧
         </Link>
-        <Link href="/admin/categories" className="p-4 block hover:bg-blue-100">
+        <Link
+          href="/admin/categories"
+          className={`p-4 block hover:bg-blue-100 ${
+            isSelected("/admin/posts") && "bg-blue-100"
+          }`}
+        >
           カテゴリー一覧
         </Link>
       </aside>
+
+      {/* メインコンテンツ */}
       <div className="ml-70 p-4">{children}</div>
     </>
   );
 };
 
-export default adminLayout;
+export default AdminLayout;
