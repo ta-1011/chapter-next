2d514a92aa448d38a10e61fdfcad7e5120290e13
kuro <kkrmlove1011@gmail.com>
2026-09-07 17:49:26 +0900

    🔧 Supabaseのホスト名の登録

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,15 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "placehold.jp",
+        pathname: "/storage/v1/object/public/**",
       },
       { protocol: "https", hostname: "images.microcms-assets.io" },
       { protocol: "https", hostname: "picsum.photos" },
+      {
+        protocol: "https",
+        hostname: "ogpbzgwgoqhmtcctkwtb.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
 };
