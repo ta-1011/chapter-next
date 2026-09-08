acf71443d2a134293ec6fb3912d4ae2fccb46104
kuro <kkrmlove1011@gmail.com>
2026-09-08 17:31:44 +0900

    🎨 エラーの修正

--- app/_components/ui/ErrorMessage.tsx
+++ app/_components/ui/ErrorMessage.tsx
@@ -1,8 +1,11 @@
+// バリデーションエラーが無いとき（正しく入力できているとき）はそもそもエラーが不要なため、？がいる
 type Props = {
-  message: string;
+  message?: string;
 };
 
 const ErrorMessage = (props: Props) => {
+  if (!props.message) return;
+
   if (props.message) {
     return <p>{props.message}</p>;
   }
